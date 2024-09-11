d9bfe6cc66c5b2841efab7545f67562f4bd01038
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-09-11 19:15:48 -0300

    criação de menu com a biblioteca inquirer

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+    while(true){
+        
+            const opcao = await select({
+                message: "Menu >",
+                choices: [
+                    {
+                        name: "Cadastrar meta",
+                        value: "cadastrar"
+                    },
+                    {
+                        name: "Listar meta",
+                        value: "listar"
+                    },
+                    {
+                        name: "Sair",
+                        value: "sair"
+                    }
+                ]
+            })
+
+        switch(opcao){
+            case "cadastrar" :
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+        }
+    }
+}
+
+start()
