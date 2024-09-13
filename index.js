5c271a5fe207ae7894748d8399deb2a990ba4434
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-09-13 15:55:56 -0300

    adiciona metodo de listar e checkbox

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 
 let meta = {
@@ -18,6 +18,32 @@ const cadastrarMeta = async () => {
     }
 
     metas.push({ value: meta, checked: false})
+}
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length== 0 ){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m)=> {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta)=>{
+        const meta = metas.find((m)=> {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log("Meta(s) marcada(s) como concluida(s)")
 }
 
 
@@ -48,7 +74,7 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 return
